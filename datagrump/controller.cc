9f87ad2db55b0d590b8b80ab7ea47a98520539c8
Ubuntu <ubuntu@sprout.csail.mit.edu>
2016-10-24 14:10:37 -0400

    Documentation

--- datagrump/controller.cc
+++ datagrump/controller.cc
@@ -5,18 +5,27 @@
 
 using namespace std;
 
+//Authors: Sam DeLaughter and Brandon Karpf
+//Last Updated: 10/24/16
+//MIT 6.829 - Fall 2016 - PSet 2
 
+/*This controller uses PID control to attempt to maintain a stable RTT with
+	high throughput when sending traffic over a cellular network with an
+	unpredictable and highly variable link capacity.
+*/
+
+//Initialize a window of 1
 int the_window_size = 1;
 
 //Set parameters
-float kp = -0.076;			//The proportional coefficient
-float ki = -0.000032;		//The integral coefficient
-float kd = -0.01;			//The derrivative coefficient
-int max_rtt = 105;
-int min_rtt = 62;
-int error_sum_bound = 600;
-int error_reset_threshold = 5;
-int error_threshold_nudge = 4;
+float kp = -0.076;				//The proportional coefficient
+float ki = -0.000032;			//The integral coefficient
+float kd = -0.01;				//The derrivative coefficient
+int max_rtt = 105;				//The maximum RTT threshold
+int min_rtt = 62;				//The minimum RTT threshold
+int error_sum_bound = 600;		//The largest +/- error_sum allowed
+int error_reset_threshold = 5;	//Amount of fuzziness for resetting error_sum
+int error_threshold_nudge = 4;	//Amount to nudge error when outside threshold
 
 //Initialize variables (don't change these values)
 int w = 1;
@@ -26,8 +35,16 @@ int error_sum = 0;
 int d_error = 0;
 int rtt_old = 0;
 
-/*Best results so far
+/*Best results achieved so far:
 	
+Average capacity: 5.04 Mbits/s
+Average throughput: 3.53 Mbits/s (70.0% utilization)
+95th percentile per-packet queueing delay: 62 ms
+95th percentile signal delay: 104 ms
+Power: 33.69 MBits/s^2 (Average Throughput / Signal Delay)
+http://cs344g.keithw.org/report/?brandonandsam-1477286548-eengaufi
+
+//Parameters Used
 float kp = -0.076;			//The proportional coefficient
 float ki = -0.00003;		//The integral coefficient
 float kd = -0.01;			//The derrivative coefficient
@@ -36,24 +53,6 @@ int min_rtt = 62;
 int error_sum_bound = 600;
 int error_reset_threshold = 5;
 int error_threshold_nudge = 4;
-
-Average capacity: 5.04 Mbits/s
-Average throughput: 3.53 Mbits/s (70.0% utilization)
-95th percentile per-packet queueing delay: 62 ms
-95th percentile signal delay: 104 ms
-Power: 33.69
-http://cs344g.keithw.org/report/?brandonandsam-1477286548-eengaufi
-*/
-
-/*
-//Best results without error_threshold_nudge
-float kp = -0.06;			//The proportional coefficient
-float ki = -0.00003;		//The integral coefficient
-float kd = -0.0205;			//The derrivative coefficient
-int max_rtt = 105;
-int min_rtt = 62;
-int error_sum_bound = 600;
-int error_reset_threshold = 5;
 */
 
 
@@ -106,25 +105,21 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
   }
   
   /*Set the error value based on the RTT's difference from the last RTT
-  and its distance from max_rtt and min_rtt
-  */
+  	and its distance from max_rtt and min_rtt*/
   int error = 0;
   if (rtt > max_rtt){
 	  /*If the RTT is above a certain threshold, set the error to
-	  the difference between the RTT and that threshold, then decrease
-	  the error slightly to nudge it back withint the threshold faster
-	  */
+	  	the difference between the RTT and that threshold, then decrease
+	  	the error slightly to nudge it back withint the threshold faster*/
 	  error = (rtt - max_rtt) - error_threshold_nudge;
   } else if (rtt < min_rtt){
 	  /*If the RTT is below a certain threshold, set the error to
-	  the difference between the RTT and that threshold, then increase
-	  the error slightly to nudge it back withint the threshold faster
-	  */
+	  	the difference between the RTT and that threshold, then increase
+	  	the error slightly to nudge it back withint the threshold faster*/
 	  error = (rtt - min_rtt) + error_threshold_nudge;
   }else{
 	  /*Otherwise, set the error to the diffence between the current RTT
-	  and the RTT from the previous ACK
-	  */
+	  	and the RTT from the previous ACK*/
       error = rtt - rtt_old;
   }
   
@@ -162,13 +157,14 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
   //Store the current RTT to reference in the next ACK's calculations
   rtt_old = rtt;
   
+  
+  /*Uncomment this block to print timestamp, RTT, and window size for each ACK
+	  
   cerr << "\n\n        Time: " << timestamp_ack_received
        << "\n      Window: " << the_window_size
        << "\n         RTT: " << rtt;
-       //<< "\n           w: " << w
-       //<< "\n   RTT Error: " << error
-       //<< "\n   Error Sum: " << error_sum
-       //<< "\nError Change: " << d_error;
+  
+  */
   
   if ( debug_ ) {
     cerr << "At time " << timestamp_ack_received
